6bf9c3b805f60293e33bb79620aa7b743f14cee3
MandyMosey <81394310+MandyMosey@users.noreply.github.com>
2021-11-22 18:38:54 -0600

    Search engine fixed

--- src/index.js
+++ src/index.js
@@ -37,12 +37,27 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function search(city) {
+  let apiKey = "f81484afceeb8c759417d745c4c551f4"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchBoxElement = document.querySelector("#search-box");
+  search(searchBoxElement.value);
 }
 
 let apiKey = "f81484afceeb8c759417d745c4c551f4"
 let city = "Boston";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+search("Boston");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
